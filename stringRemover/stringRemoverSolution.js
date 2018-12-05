02687e63161725e1107f4bc51de202e0061ea313
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-05 14:50:20 +0100

    Updated the question

--- stringRemover/stringRemoverSolution.js
+++ stringRemover/stringRemoverSolution.js
@@ -1,14 +1,17 @@
 /*Given as many arrays as possible e.g E, F, G,...., of non-negative integer scores. A "unique" score is one which is a multiple of 10, such as 50 or 60. Return the sum of largest unique score in each array. 
 
 Hint: 
-uniqueScore({12, 10, 4}, {2, 20, 30}) → 40
-uniqueScore({20, 10, 4}, {2, 20, 10}) → 40
-uniqueScore({12, 11, 4}, {2, 20, 31}) → 20
+stringRemoverSolution([12, 10, 4], [2, 20, 30]) → 40
+stringRemoverSolution([12, 10, 4], [2, 20, 30],[2, 20, 30]) → 70
+stringRemoverSolution([12, 10, 4]) → 10
 */
 
-function uniqueScore(firstArr, secondArr) {
+
   // WRITE YOUR CODE HERE
+  function stringRemoverSolution(nextArr){
+    
+   
+  }
+  
 
-}
-
-module.exports = uniqueScore;
+module.exports = stringRemoverSolution;
