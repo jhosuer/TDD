53add6dbeb0d8f9d2250a8682bc1727af639eb02
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-05 05:09:25 +0100

    Updated the question

--- stringRemover.js
+++ stringRemover.js
@@ -1,4 +1,4 @@
-/*Given two arrays, E and F, of non-negative integer scores. A "unique" score is one which is a multiple of 10, such as 50 or 60. Return the sum of largest unique score in E and the largest unique score in F. 
+/*Given as many arrays as possible e.g E, F, G,...., of non-negative integer scores. A "unique" score is one which is a multiple of 10, such as 50 or 60. Return the sum of largest unique score in each array. 
 
 Hint: 
 uniqueScore({12, 10, 4}, {2, 20, 30}) → 40
