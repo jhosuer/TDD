c33cd93c78ab0a3151ca8ed3fb9d23b886492598
techiefrankie <techiefrankie@gmail.com>
2018-12-06 12:54:20 +0100

    Solved Joshua task and made up my test cases 10

--- techie/solution.test.js
+++ techie/solution.test.js
@@ -1,4 +1,4 @@
-const concatArrays = require('./techie/solution');
+const concatArrays = require('../techie/solution');
 
 test('a simple case where the two arrays are of different lengths', () => {
     expect(concatArrays([1,5,4,6,8],[7,1,0,6])).toEqual([1,7,5,1,4,0,6,6,8]);
@@ -19,3 +19,26 @@ test('returns the other array when one array is empty', () => {
 test('a simple case of arrays of mixed elements type', () => {
     expect(concatArrays(['k',5,'5',6,'d'],['h','y'])).toEqual(['k','h',5,'y','5',6,'d']);
 });
+
+test('a simple case of arrays strings', () => {
+    expect(concatArrays(['a','c','e','h','i'],['b','d','f'])).toEqual(['a','b','c','d','e','f','h','i']);
+});
+
+test('when no argument was passed', () => {
+    expect(concatArrays()).toBe(false);
+});
+
+test('when one array argument was passed', () => {
+    expect(concatArrays([5,8,9,7,4,6])).toEqual([5,8,9,7,4,6]);
+});
+
+test('when more than two arrays argument was passed', () => {
+    expect(concatArrays([5,8,9,7,4,6],[5,7,9,5,6],[3,4,5,6])).toBe(false);
+});
+
+test('A simple case of singleton arrays', () => {
+    expect(concatArrays([5],[6])).toBe([5,6]);
+});
+
+
+
